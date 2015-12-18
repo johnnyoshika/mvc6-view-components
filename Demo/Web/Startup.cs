7e9ad59efd4495e70c00c7235f38aa02c04c42fd
Johnny Oshika <johnnyoshika@gmail.com>
2015-12-18 08:12:58 -0800

    Set up MVC6.

--- Demo/Web/Startup.cs
+++ Demo/Web/Startup.cs
@@ -15,16 +15,19 @@ namespace Web
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
-            app.UseIISPlatformHandler();
-
-            app.Run(async (context) =>
+            app.UseMvc(config =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                config.MapRoute(
+                    name: "Default",
+                    template: "{controller}/{action}/{id?}",
+                    defaults: new { controller = "Home", action = "Index" }
+                );
             });
         }
 
