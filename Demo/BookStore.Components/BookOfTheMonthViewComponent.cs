378faed650b4a603b2fb90dd3ea763b785046cac
Johnny Oshika <johnnyoshika@gmail.com>
2015-12-18 08:30:46 -0800

    Add BookOfTheMonthViewComponent.

---
+++ Demo/BookStore.Components/BookOfTheMonthViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNet.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Components
+{
+    public class BookOfTheMonthViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View((object)"Little Red Riding Hood");
+        }
+    }
+}
