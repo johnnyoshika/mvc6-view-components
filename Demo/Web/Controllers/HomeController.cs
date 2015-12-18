98f9d79ce141502e309f087ac9af43715d0c43c4
Johnny Oshika <johnnyoshika@gmail.com>
2015-12-18 08:15:40 -0800

    Add HomeController.

---
+++ Demo/Web/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNet.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
