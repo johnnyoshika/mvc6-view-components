380f8f6378a3200dd99aa9c2ccc023835172a16e
Johnny Oshika <johnnyoshika@gmail.com>
2015-12-18 08:21:42 -0800

    Add BookCatalogViewComponent in Web project.

---
+++ Demo/Web/BookCatalogViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNet.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web
+{
+    public class BookCatalogViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View(new[] {
+                "Three Little Pigs",
+                "Little Red Riding Hood",
+                "Ugly Duckling"
+            });
+        }
+    }
+}
