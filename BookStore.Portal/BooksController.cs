18a6df2727a8f0d16e8f8bee0daaddd6540fa837
Johnny Oshika <johnnyoshika@gmail.com>
2015-12-18 16:11:25 -0800

    Add BooksController in BookStore.Portal project.

---
+++ BookStore.Portal/BooksController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNet.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Portal
+{
+    public class BooksController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
