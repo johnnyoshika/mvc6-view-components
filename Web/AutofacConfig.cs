342057c7f7ba3d10916c94c743b071dd5d751714
Johnny Oshika <johnnyoshika@gmail.com>
2016-01-20 07:46:00 -0800

    Add AutofacConfig.

---
+++ Web/AutofacConfig.cs
@@ -0,0 +1,23 @@
+﻿using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web
+{
+    public static class AutofacConfig
+    {
+        public static IServiceProvider Register(IServiceCollection services)
+        {
+            var builder = new ContainerBuilder();
+            builder.Populate(services);
+
+            var container = builder.Build();
+
+            return container.ResolveOptional<IServiceProvider>();
+        }
+    }
+}
